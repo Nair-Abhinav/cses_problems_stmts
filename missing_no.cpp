63ff1d14d25622ea172db0cf17d8fe8bebe0ecb1
Nair-Abhinav <abhinavnair7404@gmail.com>
2025-01-12 08:50:54 +0530

    update

--- missing_no.cpp
+++ missing_no.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     long long n;
     cin>>n;
-    if(2<=n<=2*pow(10,6)){
+    if((n>=2) && (n<=2*pow(10,6))){
         int arr[n-1];
         for(int i=0;i<n-1;i++){
             cin>>arr[i];
